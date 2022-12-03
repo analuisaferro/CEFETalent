bb848de86468f65bc528d4e7d7a2cd428f800311
unknown <analuisaferro@gmail.com>
2022-12-02 21:26:29 -0300

    force - superfly

--- organizacao/views.py
+++ organizacao/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Prefetch
 from .forms import *
 from participante.models import Participante, Atividade, Formato_Atividade
 # Create your views here.
 
+@login_required
 def adm_painel(request):
     return render(request, 'adm_painel.html')
 
+@login_required
 def adm_recursos_listar(request):
     recursos= Recurso.objects.all()
     context = {
@@ -15,6 +18,7 @@ def adm_recursos_listar(request):
     }
     return render(request, 'adm_recursos_listar.html', context)
 
+@login_required
 def adm_cad_recurso(request):
     form_recursos = Recurso_form()
     context = {
@@ -28,6 +32,7 @@ def adm_cad_recurso(request):
             return redirect('Recursos')
     return render(request, 'adm_recursos_cad.html', context)
 
+@login_required
 def adm_participantes_listar(request):
     participantes=Participante.objects.all()
     context = {
@@ -35,16 +40,8 @@ def adm_participantes_listar(request):
     }
     return render(request, 'adm_participantes_listar.html', context)
 
+@login_required
 def adm_atividades_listar(request):
     queryset=Atividade.objects.all()
-    # atividades = []
-    # for atv in queryset:
-    #     atividades.append({
-    #     'titulo': atv.titulo,
-    #     'descricao': atv.descricao,
-    #     'formato': atv.formato_atividade.nome,
-    #     'duracao': atv.duracao,
-    #     'participantes': atv.participantes})
-    # print(atividades)
     context = {'atividades': queryset}
     return render(request, 'adm_atividades_listar.html', context)
