e7af46b8d1c1e87b3ca88bed8ced6eb8551abec4
hugobillemartins@gmail.com <hugobillemartins@gmail.com>
2022-12-10 19:35:46 -0300

    hi

--- participante/models.py
+++ participante/models.py
@@ -60,7 +60,7 @@ class Atividade(models.Model):
         ordering = ['titulo']
 
     titulo = models.CharField(max_length=64, verbose_name='Título da atividade', unique=True)
-    descricao = models.TextField(max_length=512, verbose_name='Descrição da apresentação')
+    descricao = models.TextField(max_length=1028, verbose_name='Descrição da apresentação')
     duracao = models.IntegerField(verbose_name='Duração em minutos')
     tipos_atividade = models.ManyToManyField(Tipo_Atividade)
     formato_atividade = models.ForeignKey(Formato_Atividade, on_delete=models.PROTECT)
